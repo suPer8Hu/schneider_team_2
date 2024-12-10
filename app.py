66e5cb017a0e1efac8918ab6cf2fce632377b495
changlin <changlin.hu@wisc.edu>
2024-12-10 15:46:53 -0600

    Final Commit

--- app.py
+++ app.py
@@ -51,7 +51,8 @@ import pandas as pd
 dynamodb = boto3.resource(
     'dynamodb',
     region_name='us-east-2',
-
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
 )
 
 def load_data_from_table(table_name):
